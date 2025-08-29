f81290b5401f7bf11cb40c98439d2f217d1482fa
izael.coutinho <izael.silva@tempoassist.com.br>
2025-08-29 20:59:12 -0300

    projetos criados -react - sprint boot - 1.0.0

---
+++ backendprcmnty/src/main/java/com/prcmnty/dsprcmnty/DsprcmntyApplication.java
@@ -0,0 +1,13 @@
+package com.prcmnty.dsprcmnty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprcmntyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprcmntyApplication.class, args);
+	}
+
+}
---
+++ backendprcmnty/src/test/java/com/prcmnty/dsprcmnty/DsprcmntyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prcmnty.dsprcmnty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsprcmntyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
