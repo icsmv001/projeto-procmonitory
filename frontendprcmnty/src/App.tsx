864c09b5e9f819a827f12a0818b746db3e6a3c13
izael.coutinho <izael.silva@tempoassist.com.br>
2025-09-22 23:35:11 -0300

    criado dasbord com grafico de barras e grafico de rosca - mokados - para modelo basico

--- frontendprcmnty/src/App.tsx
+++ frontendprcmnty/src/App.tsx
@@ -1,4 +1,6 @@
+import Barchart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -8,8 +10,26 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">DashBord de Processamentos ultimos 7 dias</h1>
+        <h1 className="text-primary py-3">DashBord de Processamentos ultimos 7 dias</h1>
 
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5>todas vendas</h5>
+          <Barchart/>
+        </div>
+          <div className="col-sm-6">
+          <h5>todas vendas</h5>
+          <DonutChart/>
+        </div>
+
+
+      </div>
+
+      
+      
+      <div className="py-3">
+        <h2 className="text-primary">Monitoração ultimos 7 dias</h2>
+      </div>
         <DataTable />
       </div>
 
